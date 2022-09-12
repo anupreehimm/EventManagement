9f6ffc97abb63d17ac5672ab9d67b51bdc159a8b
anupreehimm <anupreethimuruganmec@gmail.com>
2022-09-12 13:03:48 +0530

    basic create, update and delete done

--- src/main/java/manage/eventmanage/EventsExecutive.java
+++ src/main/java/manage/eventmanage/EventsExecutive.java
@@ -1,16 +1,31 @@
 package manage.eventmanage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EventsExecutive {
 	@Autowired
 	EventsService serv;
+	
+	@DeleteMapping("/dele")
+	public String callDelete(@RequestBody Events eve)
+	{
+		return serv.makeDelete(eve);
+	}
+	@GetMapping("/")
+	public List<Events> callFetching()
+	{
+		return serv.makeFetchAll();
+	}
+	
 	
 	@PostMapping("/new")
 	public Events callCreate(@RequestBody Events eve) {
--- src/main/java/manage/eventmanage/EventsService.java
+++ src/main/java/manage/eventmanage/EventsService.java
@@ -1,12 +1,25 @@
 package manage.eventmanage;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EventsService {
 	@Autowired
 	EventsInterface repo;
+	
+	
+	public String makeDelete(Events eve)
+	{
+		String msg=eve.getEveLocation()+"has been Deleted";
+		repo.delete(eve);
+		return msg;
+	}
+	
+	public List<Events> makeFetchAll(){
+		return (List<Events>) repo.findAll();
+	}
 	
 	public Events makeAdd(Events eve) {
 		return repo.save(eve);
